2af0c6118f05bad65eefe78a12af7f104638a3a9
Archit Porwal <architporwal@gmail.com>
2023-10-02 00:58:40 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,27 @@
+/*
+* PARCEL?
+* HMR - Hot Module Reload - Used by Parcel to reload the page, on any change in the code.
+* Parcel used File Watcher Algorithm (written in C++).
+* BUNDLING
+* MINIFY
+* Cleaning our code
+* Dev and Production Builds
+* Super fast Build Algorithm
+* Image Optimization
+* Caching while development     
+* Compression
+* Compatible with older version of browsers.
+* HTTPS on Dev
+* Port Number
+* Consistent Hashing Algorithm
+* Zero Config
+*/
+
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
+const heading = React.createElement("h1", {}, "Namaste Everyone!");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
