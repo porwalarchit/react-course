2ac19195baff66382843489a4a5f1c54f2b6ba50
Archit Porwal <aporwal@argusoft.com>
2023-12-13 14:35:13 +0530

    Added Search Restaurants functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,22 +2,33 @@ import RestaurantCard from "./RestaurantCard";
 import { restaurantsData } from "../utils/mockData";
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
+import { SWIGGY_CDN } from "../utils/constants";
 
 const Body = () => {
   const [restaurantData, setRestaurantData] = useState([]);
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
     fetchData();
   }, []);
 
   const fetchData = async () => {
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.2156354&lng=72.63694149999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-    );
+    const data = await fetch(SWIGGY_CDN);
 
     const jsonData = await data.json();
+
     // Optional Chaining
+    console.log(
+      jsonData?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
     setRestaurantData(
+      jsonData?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+    setFilteredRestaurants(
       jsonData?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
         ?.restaurants
     );
@@ -33,21 +44,48 @@ const Body = () => {
   ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+              const filteredData = restaurantData.filter((res) => 
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+
+              setFilteredRestaurants(filteredData);
+            }}
+          />
+          <button
+            className="search-btn"
+            onClick={() => {
+              const filteredData = restaurantData.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+
+              setFilteredRestaurants(filteredData);
+            }}
+          >
+            Search
+          </button>
+        </div>
+
         <button
           className="filter-btn"
           onClick={() => {
             filteredData = restaurantData.filter(
               (res) => res.info.avgRating >= 4
             );
-            setRestaurantData(filteredData);
-            // console.log(filteredData);
+            setFilteredRestaurants(filteredData);
           }}
         >
           Top Rated Restaurants
         </button>
       </div>
       <div className="restaurant-list">
-        {restaurantData.map((restaurant) => {
+        {filteredRestaurants.map((restaurant) => {
           return (
             <RestaurantCard {...restaurant.info} key={restaurant.info.id} />
           );
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,6 @@ export const CDN_URL =
 
 export const LOGO_URL =
   "https://play-lh.googleusercontent.com/A8jF58KO1y2uHPBUaaHbs9zSvPHoS1FrMdrg8jooV9ftDidkOhnKNWacfPhjKae1IA";
+
+export const SWIGGY_CDN =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.2156354&lng=72.63694149999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
